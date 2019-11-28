35ca2384e34e2a772634cd7dfe8c909204115762
Ramzanzad Malek <kaveh.ramzanzad@gmail.com>
2019-11-28 02:41:30 +0100

    Updates

---
+++ precache-manifest.50ecd4739fb00a891c5b3857935481ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de43365fdf55e2454c480fa06b8e861d",
+    "url": "/RoboFriend/index.html"
+  },
+  {
+    "revision": "fdd29413e4df4b582060",
+    "url": "/RoboFriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b59389853eae7e5fe383",
+    "url": "/RoboFriend/static/css/main.564d1eff.chunk.css"
+  },
+  {
+    "revision": "fdd29413e4df4b582060",
+    "url": "/RoboFriend/static/js/2.86b2685e.chunk.js"
+  },
+  {
+    "revision": "b59389853eae7e5fe383",
+    "url": "/RoboFriend/static/js/main.e7999570.chunk.js"
+  },
+  {
+    "revision": "ae770bb6d2375b93589f",
+    "url": "/RoboFriend/static/js/runtime-main.6219b8e4.js"
+  }
+]);
